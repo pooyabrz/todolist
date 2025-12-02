7f4e6d9372a7f315ea71cc76ec5ebe4b8713226f
Pooya <barazandehpooya@gmail.com>
2025-12-02 13:43:22 +0330

    fix bugs

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from src.todolist.api.routers.task_router import router as task_router
 app = FastAPI(
     title="To Do List Project (Phase 3 - Web API)",
     description="FastAPI",
-<<<<<<< HEAD
     version="3.0.0",
-=======
-    version="1.0.0",
->>>>>>> bc1d1638349c56f6913be85b56ccf55ab5d74a68
     docs_url="/docs",
     redoc_url="/redoc"
 )
--- src/todolist/api/schemas.py
+++ src/todolist/api/schemas.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, Field
 
-<<<<<<< HEAD
 class CategoryResponse(BaseModel):
     id: int
     name: str
@@ -16,12 +15,6 @@ class TaskBase(BaseModel):
     due_date: Optional[datetime] = None
     priority: int = Field(2, ge=1, le=3, description="1=Low, 2=Medium, 3=High")
     category_name: Optional[str] = Field(None, max_length=100)
-=======
-class TaskBase(BaseModel):
-    title: str = Field(..., min_length=3, max_length=200, description="عنوان وظیفه")
-    description: Optional[str] = Field(None, max_length=1000, description="توضیحات")
-    due_date: Optional[datetime] = Field(None, description="تاریخ سررسید (ISO format)")
->>>>>>> bc1d1638349c56f6913be85b56ccf55ab5d74a68
 
 class TaskCreate(TaskBase):
     pass
@@ -30,23 +23,16 @@ class TaskUpdate(BaseModel):
     title: Optional[str] = Field(None, min_length=3, max_length=200)
     description: Optional[str] = Field(None, max_length=1000)
     due_date: Optional[datetime] = None
-<<<<<<< HEAD
     priority: Optional[int] = Field(None, ge=1, le=3)
     category_name: Optional[str] = None
-=======
-    is_completed: Optional[bool] = None
->>>>>>> bc1d1638349c56f6913be85b56ccf55ab5d74a68
 
 class TaskResponse(TaskBase):
     id: int
     is_completed: bool
     created_at: datetime
     updated_at: Optional[datetime]
-<<<<<<< HEAD
     completed_at: Optional[datetime] = None
     category: Optional[CategoryResponse] = None
-=======
->>>>>>> bc1d1638349c56f6913be85b56ccf55ab5d74a68
 
     class Config:
         from_attributes = True
